a68d85cc3aeff690f5251859f6473026a19c1cf5
Rizki Awal R <rizkiawal16@gmail.com>
2022-06-11 10:17:53 +0700

    second

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 module.exports = {
-    multipleStatements  : true,
-    host                : "sql6.freemysqlhosting.net",
-    user                : "sql6498920",
-    password            : "atwEmgTqwj",
-    database            : "sql6498920"
+    multipleStatements: true,
+    host: "sql6.freemysqlhosting.net",
+    user: "sql6499011",
+    password: "kGR1hV6TDu",
+    database: "sql6499011"
 };
