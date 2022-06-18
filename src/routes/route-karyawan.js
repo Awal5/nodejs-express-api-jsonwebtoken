7f7580ea1c35a613ae8bb0e0e337aa7bc404f10f
Rizki Awal R <rizkiawal16@gmail.com>
2022-06-18 09:18:57 +0700

    ganti token

--- src/routes/route-karyawan.js
+++ src/routes/route-karyawan.js
@@ -3,22 +3,20 @@ const jwt = require('jsonwebtoken');
 const router = require('express').Router();
 const { karyawan } = require('../controllers');
 
-function AuthenticateAccessToken(req,res,next){
+function AuthenticateAccessToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[2];
     console.log(process.env.ACCESS_TOKEN_SECRET);
     console.log(token);
-    
-    if(token == null){
-        res.json({ message: 'Invalid access token'});
-    }
-    else{
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,user) => {
-            if(err){
+
+    if (token == null) {
+        res.json({ message: 'Invalid access token' });
+    } else {
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+            if (err) {
                 res.json({ message: err });
-            }
-            else{
-                
+            } else {
+
                 next();
             }
         });
@@ -26,13 +24,13 @@ function AuthenticateAccessToken(req,res,next){
 }
 
 // GET localhost:8080/karyawan => Ambil data semua karyawan
-router.get('/karyawan', AuthenticateAccessToken, karyawan.getDataKaryawan);
+router.get('/karyawan', karyawan.getDataKaryawan);
 
 // GET localhost:8080/karyawan/2 => Ambil data semua karyawan berdasarkan id = 2
 router.get('/karyawan/:id', karyawan.getDataKaryawanByID);
 
 // POST localhost:8080/karyawan/add => Tambah data karyawan ke database
-router.post('/karyawan/add', karyawan.addDataKaryawan);
+router.post('/karyawan/add', AuthenticateAccessToken, karyawan.addDataKaryawan);
 
 // POST localhost:8080/karyawan/2 => Edit data karyawan
 router.post('/karyawan/edit', karyawan.editDataKaryawan);
